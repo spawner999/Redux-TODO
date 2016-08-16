1b7be37bf5e1e977b228e6be3144c26db8ca8a3a
Lorenzo <lferrario@byte-code.com>
2016-08-16 17:45:14 +0200

    normalize the namespace, now using lookup tables to store todos

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,51 +1,48 @@
-const todo = (state, action) => {
+import { combineReducers } from 'redux';
+import todo from './todo';
+
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      };
     case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action), //computed property
       }
-      return {
-        ...state,
-        completed: !state.completed,
-      };
-    default:
-      return state;
-
-  }
-};
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action),
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      );
     default:
     return state;
   }
 };
 
+//lookup table
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id];
+    default:
+      return state;
+  }
+};
+
+const todos = combineReducers({
+  byId,
+  allIds
+});
+
 export default todos;//reducer function
 
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]); // map all the ids from allIds to the corresponding todos from byId
+
 export const getVisibleTodos = (state, filter) => { // selector function, it selects something from the current state
+  const allTodos = getAllTodos(state);
   switch (filter) {
     case 'all':
-      return state;
+      return allTodos;
     case 'active':
-      return state.filter(t => !t.completed);
+      return allTodos.filter(t => !t.completed);
     case 'completed':
-      return state.filter(t => t.completed);
+      return allTodos.filter(t => t.completed);
     default:
       throw new Error(`Unknown filter: ${filter}`);
   }

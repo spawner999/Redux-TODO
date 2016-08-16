32cd5b9c4c8254e7667712b68003eb4678627941
Lorenzo <lferrario@byte-code.com>
2016-08-16 18:09:22 +0200

    add logs when actions are dispatched

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,9 +3,31 @@ import todoApp from './reducers';
 import { loadState , saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action); // dispatch is synchronous
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(todoApp, persistedState);
+
+  //only call in development mode
+  if(process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   //throttle ensures the inner function is not called too often
   store.subscribe(throttle(() => {

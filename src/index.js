55155e4548a27a1a749a46b547c09f7b6f040c1a
lorenzo ferrario <lorenzoferrario@lorenzos-MacBook-Pro.local>
2016-08-16 15:02:08 +0200

    add implementation up first lesson of the course

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import App from './components/App';
 
-ReactDOM.render(
-  <App />,
+const store = createStore(todoApp);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

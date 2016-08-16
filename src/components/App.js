e030df1430d139b4baaa80895455db507bb16012
Lorenzo <lferrario@byte-code.com>
2016-08-16 16:33:18 +0200

    add withRouter to inject params directly into the connect component instead of passing them down from the route component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import AddTodo from './AddTodo';
 import Footer from './Footer';
 import VisibleTodoList from './VisibleTodoList';
 
-const App = ({ params }) => ( //available from the route handler
+const App = () => (
   <div>
     <AddTodo />
-    <VisibleTodoList filter={params.filter || 'all' }/> 
+    <VisibleTodoList />
     <Footer />
   </div>
 );

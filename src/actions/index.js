15bafade0a29af9d1c569f778acc45ba6f521348
lorenzo ferrario <lorenzoferrario@lorenzos-MacBook-Pro.local>
2016-08-16 15:31:13 +0200

    add persisting state and localstorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
-let nextTodoId = 0;
+import { v4 } from 'node-uuid'; //create serialized id
+
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
-  id: (nextTodoId++).toString(),
+  id: v4(),
   text,
 });
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState , saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
-const store = createStore(todoApp);
+const persistedState = loadState();
+const store = createStore(todoApp, persistedState);
+
+//throttle ensures the inner function is not called too often
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos // just the todos not the filter
+  });
+}, 1000));
 
 render(
   <Provider store={store}>

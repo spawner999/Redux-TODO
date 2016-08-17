27ebb95397857f9c3bcbfd94f013c16c876f5a61
Lorenzo <lferrario@byte-code.com>
2016-08-17 12:12:17 +0200

    refactor using redux applyMiddleware and external middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,53 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-//override store.dispatch to show more information
-//function that return a functions that returns a function, common in functional programming, it's called currying
-const logger = (store) => {
-  return (next) => {
-    if (!console.group) {
-      return next;
-    }
-
-    return (action) => {
-      console.group(action.type);
-      console.log('%c prev state', 'color: gray', store.getState());
-      console.log('%c action', 'color: blue', action);
-      const returnValue = next(action); // dispatch is synchronous
-      console.log('%c next state', 'color: green', store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-    };
-  };
-};
-
-//same here, think of it as function with different arguments that are applied as they become available
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') { //if the then field of the action is a function (action is a promise)
-    return action.then(next);  //resolve and then normal dispatch
-  }
-  return next(action); //it's a normal action so just dispatch it
-};
-
-//overrides dispatch multiple times
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => //revert middleware to apply promise last
-    store.dispatch = middleware(store)(store.dispatch) //store.dispatch would be the 'next' argument
-  );
-};
 
 const configureStore = () => {
-  const store = createStore(todoApp);
   const middlewares =[promise];
-
   if(process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store; //return a store ready to be used
+  return createStore(
+    todoApp,
+    //persistedState goes here if present
+    applyMiddleware(...middlewares) //this argument is called enhancer
+  );
 }
 
 export default configureStore;

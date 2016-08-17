250941a88975bae0772c109023fc56c40fb3a2ff
Lorenzo <lferrario@byte-code.com>
2016-08-17 12:02:20 +0200

    manually implement middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,44 +2,50 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 
 //override store.dispatch to show more information
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
+//function that return a functions that returns a function, common in functional programming, it's called currying
+const logger = (store) => {
+  return (next) => {
+    if (!console.group) {
+      return next;
+    }
 
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action); // dispatch is synchronous
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
+    return (action) => {
+      console.group(action.type);
+      console.log('%c prev state', 'color: gray', store.getState());
+      console.log('%c action', 'color: blue', action);
+      const returnValue = next(action); // dispatch is synchronous
+      console.log('%c next state', 'color: green', store.getState());
+      console.groupEnd(action.type);
+      return returnValue;
+    };
   };
 };
 
-//override store.dispatch to support async / promises
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') { //if the then field of the action is a function (action is a promise)
-      return action.then(rawDispatch);  //resolve and then normal dispatch
-    }
-    return rawDispatch(action); //it's a normal action so just dispatch it
-  };
+//same here, think of it as function with different arguments that are applied as they become available
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') { //if the then field of the action is a function (action is a promise)
+    return action.then(next);  //resolve and then normal dispatch
+  }
+  return next(action); //it's a normal action so just dispatch it
+};
+
+//overrides dispatch multiple times
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => //revert middleware to apply promise last
+    store.dispatch = middleware(store)(store.dispatch) //store.dispatch would be the 'next' argument
+  );
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares =[promise];
 
-  //only call in development mode
   if(process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  //important to override after the other override
-  store.dispatch = addPromiseSupportToDispatch(store);
+
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store; //return a store ready to be used
 }

cba290bc03282c923b3ba12f1c4b14315bfd4385
Lorenzo <lferrario@byte-code.com>
2016-08-17 10:22:25 +0200

    fetch data on route change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,12 +1,37 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
-import { getVisibleTodos } from '../reducers/index';
+import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
 
-const mapStateToProps = (state, { params }) => ({
-  todos: getVisibleTodos(state, params.filter || 'all'),
-});
+//making this a react component so that it's possible to access its lifecycle and make the api call there
+class VisibleTodoList extends Component {
+  componentDidMount(){
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  };
+  componentDidUpdate(prevProps){
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    };
+  };
+  render() {
+    return <TodoList {... this.props} />;
+  }
+};
+
+const mapStateToProps = (state, { params }) => {
+  const filter = params.filter || 'all'; //explicitly saving the filter
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  }
+};
 
 // const mapDispatchToProps = (dispatch) => ({
 //   onTodoClick(id) {
@@ -14,9 +39,9 @@ const mapStateToProps = (state, { params }) => ({
 //   }
 // });
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo } //mapping object used in place of mapDispatchToProps since the arguments used are the same (id)
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
